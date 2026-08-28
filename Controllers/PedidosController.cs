15c9182835bc3836dcc9b3b11df03766e387ed8c
mariel777q <apalamariel8@gmail.com>
2026-08-28 12:43:28 -0400

    Modificacion de autorizacion

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ using RestauranteSaborCasero.Models;
 
 namespace RestauranteSaborCasero.Controllers
 {
+    [Authorize]
     public class PedidosController : Controller
     {
         private readonly ApplicationDbContext _context;
